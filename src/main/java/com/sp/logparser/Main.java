85fe5aaf6d0f660eeb0e95118503127d2ac6802d
shapanmail <67509512+shapanmail@users.noreply.github.com>
2021-08-20 09:00:09 +1000

    remove unnecessary code.

--- src/main/java/com/sp/logparser/Main.java
+++ src/main/java/com/sp/logparser/Main.java
@@ -5,15 +5,9 @@ import com.sp.logparser.input.FileInputReader;
 import com.sp.logparser.input.InputReader;
 
 public class Main {
-    static String rec = "177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] \"GET /intranet-analytics/ HTTP/1.1\" 200 3574 \"-\" \"Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7\"";
-
+    
     public static void main(String[] args) {
-//        InputReader reader = new InputReader() {
-//            @Override
-//            public List<String> readInput() {
-//                return List.of(rec);
-//            }
-//        };
+
         InputReader reader = new FileInputReader("src/main/resources/log.txt");
         LogParser parser = new LogParser(reader);
         parser.parse();
